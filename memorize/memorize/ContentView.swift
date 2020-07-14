f484d5b0bdab932648da8fa380df2e50a0e05667
Louis Ye <yel@carleton.edu>
2020-07-14 00:42:39 -0700

    lecture 1 follow along

---
+++ memorize/memorize/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Louis Ye on 7/14/20.
+//  Copyright © 2020 Louis Ye. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            ForEach(0..<4){ index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View{
+    var isFaceUp: Bool
+    var body: some View{
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            }else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
