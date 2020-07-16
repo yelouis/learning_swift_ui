a3f346cdb88da799a422d67b9a1bf7dbeb91b591
Louis Ye <yel@carleton.edu>
2020-07-15 17:37:45 -0700

    Finished 3rd lecture and got an updating interface working

--- memorize/memorize/EmojiMemoryGameView.swift
+++ memorize/memorize/EmojiMemoryGameView.swift
@@ -21,7 +21,6 @@ struct EmojiMemoryGameView: View {
         }
         .padding()
         .foregroundColor(Color.orange)
-        .font(Font.largeTitle)
     }
 }
 
@@ -29,19 +28,34 @@ struct CardView: View{
     var card: MemoryGame<String>.Card
     
     var body: some View{
-        GeometryReader(content: { geometry in
-            ZStack{
-                if self.card.isFaceUp {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                    Text(card.content)
-                }else{
-                    RoundedRectangle(cornerRadius: 10.0).fill()
-                }
-            }
-        })
+        GeometryReader{ geometry in
+            self.body(for: geometry.size)
+        }
 
     }
+    
+    func body(for size: CGSize) -> some View{
+        ZStack{
+            if self.card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(self.card.content)
+            }else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    
+    func fontSize(for size: CGSize) -> CGFloat{
+        min(size.width, size.height)*0.75
+    }
+    
 }
 
 
